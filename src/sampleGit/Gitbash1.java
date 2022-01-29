44ad4f678f430891e3abaf86a6bec514e5e0b4fa
KalpanaShetty <kalpanashetty512@gmail.com>
2022-01-29 13:35:40 +0530

    sample2 automated

--- src/sampleGit/Gitbash1.java
+++ src/sampleGit/Gitbash1.java
@@ -5,6 +5,7 @@ public class Gitbash1 {
 	public static void main(String[] args) {
 		System.out.println("hi world");
 		System.out.println("second change");
+	System.out.println("third change in first class");
 	}
 
 }
---
+++ src/sampleGit/Sample2.java
@@ -0,0 +1,10 @@
+package sampleGit;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		System.out.println("second file created");
+        System.out.println("adding one more extra line");
+	}
+
+}
